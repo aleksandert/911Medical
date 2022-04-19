534f91258c6ac51c95180279891ead3d0e809d23
aleksander_tusek <aleksander@sowalabs.com>
2022-04-19 07:24:56 +0200

    Removing obsolete projects

--- 911Medical.MvcApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _911Medical.MvcApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
